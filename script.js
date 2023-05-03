5d5a70f59b64165ca016dfb67f4b8e9b17116579
Sahran Prasla <sahranprasla@Sahrans-MacBook-Pro.local>
2023-05-02 19:18:40 -0500

    Function waits for key press before executing script.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 (function() {
-  setTimeout(function(){ 
-    var sound = new Audio("http://127.0.0.1:5500/sound/shriek.wav")
-    sound.play()
-    document.body.innerHTML = '';
-    const img = document.createElement("img");
-    img.src = "images/creep.jpeg";
-    document.body.appendChild(img);
-    document.body.style.background = "black";
-    console,log('Here!')
-    }, 3000);
+  document.addEventListener("keydown", (event) => {
     setTimeout(function(){ 
-      location.reload();
-    }, 8000);
+      var sound = new Audio("http://127.0.0.1:5500/sound/shriek.wav")
+      sound.play()
+      document.body.innerHTML = '';
+      const img = document.createElement("img");
+      img.src = "images/creep.jpeg";
+      document.body.appendChild(img);
+      document.body.style.background = "black";
+      console,log('Here!')
+      }, 3000);
+      setTimeout(function(){ 
+        location.reload();
+      }, 8000);
+})
 }());
